21901176fdb50aac6a5d91c4a2acbb6176d5cf77
mnrojas2 <mnrojas2@uc.cl>
2023-10-19 11:40:14 -0300

    Update files to allow distance and time filtering

--- just_calibration.py
+++ just_calibration.py
@@ -1,19 +1,49 @@
 import argparse
 import numpy as np
-
-
 import cv2 as cv
 import pickle
+from scipy.spatial.transform import Rotation as R
 
 # Initialize parser
 parser = argparse.ArgumentParser(description='Camera calibration using chessboard images.')
 parser.add_argument('file', type=str, help='Name of the file containing data (*pkl).')
 parser.add_argument( '-e', '--extended', action='store_true', default=False, help='Enables use of cv.calibrateCameraExtended instead of the default function.')
-parser.add_argument( '-s', '--save', action='store_true', default=False, help='Saves calibration data results in .yml format as well as TARGET position data in .txt format.')
-parser.add_argument('-rd', '--reduction', type=int, metavar='N', default=1, help='Reduction of number of frames (total/N).')
-parser.add_argument('-rs', '--residue', type=int, metavar='N', default=0, help='Residue or offset for the reduced number of frames.')
+parser.add_argument( '-s', '--save', action='store_true', default=False, help='Saves calibration data results in .yml format.')
+parser.add_argument('-fd', '--filterdist', action='store_true', default=False, help='Enables filter by distance of camera position.')
+parser.add_argument('-ft', '--filtertime', action='store_true', default=False, help='Enables filter by time between frames.')
+parser.add_argument('-md', '--mindist', type=float, metavar='N', default=0.0, help='Minimum distance between cameras (available only when --filterdist is active).')
+parser.add_argument('-rd', '--reduction', type=int, metavar='N', default=1, help='Reduction of number of frames (total/N) (available only when --filtertime is active).')
+parser.add_argument('-rs', '--residue', type=int, metavar='N', default=0, help='Residue or offset for the reduced number of frames (available only when --filtertime is active).')
 
+###############################################################################################################################
+# Functions
+
+def split_by_distance(objpts, imgpts, names, vecs, min_dist=150):
+    # Get distance of the camera between frames using rvec and tvec and return the lists of frames with a difference over "min_dist".
+    arg_split = []
+    for i in range(len(vecs)):
+        rvec = vecs[i][0]
+        tvec = vecs[i][1]
+        rmat = R.from_rotvec(rvec.reshape(3))
+        tmat = np.dot(rmat.as_matrix(), tvec)
+        if i == 0:
+            tmat_old = tmat
+            arg_split.append(i)
+        else:
+            dtc = np.linalg.norm(tmat_old - tmat)
+            if dtc >= min_dist:
+                tmat_old = tmat
+                arg_split.append(i)
+    
+    # After getting all frames with a significant distance, filter the 3D, 2D and name lists to have only them.  
+    nobj = [objpts[i] for i in range(len(objpts)) if i in arg_split]
+    nimg = [imgpts[i] for i in range(len(imgpts)) if i in arg_split]
+    nnames = [names[i] for i in range(len(names)) if i in arg_split]
+    return nobj, nimg, nnames
+
+###############################################################################################################################
 # Flags
+
 # CALIB_USE_INTRINSIC_GUESS: Calibration needs a preliminar camera_matrix to start (necessary in non-planar cases)
 flags_model = cv.CALIB_USE_INTRINSIC_GUESS
 
@@ -23,24 +53,34 @@ flags_model = cv.CALIB_USE_INTRINSIC_GUESS
 # Get parser arguments
 args = parser.parse_args()
 
-rd = args.reduction
-rs = args.residue
-
 # Load pickle file
+print(f'Loading {args.file}.pkl')
 pFile = pickle.load(open(f"./tests/points-data/tracked/{args.file}.pkl","rb"))
 
-# Assign lists
-objpoints = pFile['3D_points'][rs::rd]
-imgpoints = pFile['2D_points'][rs::rd]
-ret_names = pFile['name_points'][rs::rd]
+# Unpack lists
+objpoints = pFile['3D_points']
+imgpoints = pFile['2D_points']
+ret_names = pFile['name_points']
 
 camera_matrix = pFile['init_mtx']
 dist_coeff = pFile['init_dist']
 img_shape = pFile['img_shape']
 
 calibfile = pFile['init_calibfile']
-rvec = np.array(pFile['rt_vectors'][0])
-tvec = np.array(pFile['rt_vectors'][1])
+vecs = pFile['rt_vectors']
+
+# Filter lists if required
+if args.filterdist:
+    print(f'Filter by distance enabled')
+    objpoints, imgpoints, ret_names = split_by_distance(objpoints, imgpoints, ret_names, vecs, args.mindist)
+    
+elif args.filtertime:
+    print(f'Filter by time enabled')
+    objpoints = objpoints[args.residue::args.reduction]
+    imgpoints = imgpoints[args.residue::args.reduction]
+    ret_names = ret_names[args.residue::args.reduction]
+    
+print(f'Length of lists for calibration: {len(ret_names)}')
 
 # Camera Calibration
 print("Calculating camera parameters...")
@@ -70,4 +110,4 @@ if args.save:
         fs.write('per_view_errors', pVElist)
     fs.release()
 
-print("We finished!") # '''
+print("We finished!")
--- point_tracking.py
+++ point_tracking.py
@@ -13,6 +13,7 @@ import pickle
 from tqdm import tqdm
 from matplotlib import pyplot as plt
 from scipy.spatial import distance
+from scipy.spatial.transform import Rotation as R
 
 # Initialize parser
 parser = argparse.ArgumentParser(description='Camera calibration using chessboard images.')
@@ -21,11 +22,15 @@ parser.add_argument('-cb', '--calibfile', type=str, metavar='file', help='Name o
 parser.add_argument( '-p', '--plot', action='store_true', default=False, help='Shows or saves plots of every frame with image points and projected points.')
 parser.add_argument( '-o', '--outf', type=str, metavar='out-folder', help='Name of the folder containing frames with their points (--plot must also be called).')
 parser.add_argument('-hf', '--halfway', type=str, metavar='target_data', help='Name of the file containing target data to restart tracking process from any frame (*.txt).')
+parser.add_argument( '-s', '--save', action='store_true', default=False, help='Saves TARGET position data in .txt format as well as vectors of the 3D and 2D points for calibration.')
+parser.add_argument('-fd', '--filterdist', action='store_true', default=False, help='Enables filter by distance of camera position.')
+parser.add_argument('-ft', '--filtertime', action='store_true', default=False, help='Enables filter by time between frames.')
+parser.add_argument('-md', '--mindist', type=float, metavar='N', default=0.0, help='Minimum distance between cameras (available only when --filterdist is active).')
+parser.add_argument('-rd', '--reduction', type=int, metavar='N', default=1, help='Reduction of number of frames (total/N) (available only when --filtertime is active).')
+parser.add_argument('-rs', '--residue', type=int, metavar='N', default=0, help='Residue or offset for the reduced number of frames (available only when --filtertime is active).')
 parser.add_argument( '-c', '--calibenable', action='store_true', default=False, help='Enables calibration process after finding all points from video.')
 parser.add_argument( '-e', '--extended', action='store_true', default=False, help='Enables use of cv.calibrateCameraExtended instead of the default function.')
-parser.add_argument('-rd', '--reduction', type=int, metavar='N', default=1, help='Reduction of number of frames (total/N).')
-parser.add_argument('-rs', '--residue', type=int, metavar='N', default=0, help='Residue or offset for the reduced number of frames.')
-parser.add_argument( '-s', '--save', action='store_true', default=False, help='Saves calibration data results in .yml format as well as TARGET position data in .txt format.')
+parser.add_argument('-cs', '--calibsave', action='store_true', default=False, help='Saves calibration data results in .yml format.')
 
 
 ###############################################################################################################################
@@ -102,6 +107,29 @@ def deleteFarPoints(dataframe, df_projected, limit=60):
     new_df['dist'] = np.linalg.norm(new_df.iloc[:, [1,2]].values - new_dfXY, axis=1)
     new_df_list = new_df.loc[new_df['dist'] >= limit]['index'].to_list()
     return dataframe.drop(new_df_list)
+
+def split_by_distance(objpts, imgpts, names, vecs, min_dist=150):
+    # Get distance of the camera between frames using rvec and tvec and return the lists of frames with a difference over "min_dist".
+    arg_split = []
+    for i in range(len(vecs)):
+        rvec = vecs[i][0]
+        tvec = vecs[i][1]
+        rmat = R.from_rotvec(rvec.reshape(3))
+        tmat = np.dot(rmat.as_matrix(), tvec)
+        if i == 0:
+            tmat_old = tmat
+            arg_split.append(i)
+        else:
+            dtc = np.linalg.norm(tmat_old - tmat)
+            if dtc >= min_dist:
+                tmat_old = tmat
+                arg_split.append(i)
+                
+    # After getting all frames with a significant distance, filter the 3D, 2D and name lists to have only them.  
+    nobj = [objpts[i] for i in range(len(objpts)) if i in arg_split]
+    nimg = [imgpts[i] for i in range(len(imgpts)) if i in arg_split]
+    nnames = [names[i] for i in range(len(names)) if i in arg_split]
+    return nobj, nimg, nnames
 
 ###############################################################################################################################
 # Parameters
@@ -377,12 +405,18 @@ if args.save:
 # When everything done, release the frames
 cv.destroyAllWindows()
 
-# reduction
-rd = args.reduction
-rs = args.residue
-objpoints = objpoints[rs::rd]
-imgpoints = imgpoints[rs::rd]
-ret_names = ret_names[rs::rd]
+# Filter lists if required
+if args.filterdist:
+    print(f'Filter by distance enabled')
+    objpoints, imgpoints, ret_names = split_by_distance(objpoints, imgpoints, ret_names, vecs, args.mindist)
+    
+elif args.filtertime:
+    print(f'Filter by time enabled')
+    objpoints = objpoints[args.residue::args.reduction]
+    imgpoints = imgpoints[args.residue::args.reduction]
+    ret_names = ret_names[args.residue::args.reduction]
+    
+print(f'Length of lists for calibration: {len(ret_names)}')
 
 # Camera Calibration
 if args.calibenable:
@@ -399,7 +433,7 @@ if args.calibenable:
     if args.extended:
         print('Error per frame:\n', pVE_extended)
 
-    if args.save:
+    if args.calibsave:
         summary = input("Insert comments: ")
         fs = cv.FileStorage('./tests/results/'+args.folder[:-4]+'.yml', cv.FILE_STORAGE_WRITE)
         fs.write('summary', summary)
