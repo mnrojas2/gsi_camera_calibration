ee20a8fa78390deab36f757700aa08d7672c2b06
mnrojas2 <mnrojas2@uc.cl>
2023-10-24 13:03:45 -0300

    Add subplot support and more filter options

--- tabulate_yml_files.py
+++ tabulate_yml_files.py
@@ -7,6 +7,7 @@ import re
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import gridspec
 from scipy.optimize import curve_fit
 
 # Initialize parser
@@ -16,58 +17,71 @@ def gaus(X,C,X_mean,sigma):
     # Calculate the Gaussian PDF values given Gaussian parameters and random variable X
     return C*np.exp(-(X-X_mean)**2/(2*sigma**2))
 
-def df_histogram(dataframe, colname, idx_tuple, gauss_c=False):
-    idx_filt = idx_tuple[0]
-    idx_type = idx_tuple[1]
-    
-    # Find rows if a particular str appears in summary or index
-    if idx_type == 'index':
-        df_idx = dataframe.index.str.contains(idx_filt, na=False)
-    elif idx_type == 'summary':
-        df_idx = dataframe.summary.str.contains(idx_filt, na=False)
+def df_histogram(dataframe, colname, *args, gauss_c=False):
+    for arg in args:
+        idx_filt = arg[0]
+        idx_type = arg[1]
+        
+        # Find rows if a particular str appears in summary or index
+        if idx_type == 'index':
+            df_idx = dataframe.index.str.contains(idx_filt, na=False)
+        elif idx_type == 'summary':
+            df_idx = dataframe.summary.str.contains(idx_filt, na=False).to_numpy()
+
+        # If there are more than just one arg, then add them bitwise
+        if arg == args[0]:
+            df_idx_s = df_idx
+        else:
+            df_idx_s = df_idx_s & df_idx
     
     # Filter the original dataframe and calculate mean and std
-    new_df = dataframe[df_idx][colname]
+    new_df = dataframe[df_idx_s][colname]
+    new_dfs = dataframe[df_idx_s]['summary']
     new_df_ds = new_df.describe()
     new_df_ds.loc['std/mean'] = new_df_ds.loc['std']/new_df_ds.loc['mean']
-    print(new_df, '\n', new_df_ds)
+    print(pd.concat([pd.concat([new_df, new_dfs], axis=1), new_df_ds]))
     
-    # Generate histogram
+    # Convert dataframe to numpy array
     x_data = new_df.to_numpy(dtype='float32')
     
-    # fig = plt.figure()
-    fig, axs = plt.subplots(x_data.shape[1])
+    # Define subplots positions
+    cols = 3 if x_data.shape[1] >= 5 or x_data.shape[1] == 3 else 2 
+    cols = cols if x_data.shape[1] != 1 else 1
+    rows = int(np.ceil(x_data.shape[1] / cols))
+    gs = gridspec.GridSpec(rows, cols)
+    fig = plt.figure()
+    
+    # Generate histograms and add to subplots
     for i in range(x_data.shape[1]):
         hist, bin_edges = np.histogram(x_data[:,i])
         y_hist = hist/sum(hist)
+        x_hist = np.zeros((len(y_hist)),dtype=float) 
         
-        n = len(y_hist)
-        x_hist = np.zeros((n),dtype=float) 
-        for ii in range(n):
+        for ii in range(len(y_hist)):
             x_hist[ii] = (bin_edges[ii+1]+bin_edges[ii])/2
 
         mean = sum(x_hist*y_hist)/sum(y_hist)                  
         sigma = sum(y_hist*(x_hist-mean)**2)/sum(y_hist) 
-
-        # Gaussian least-square fitting process
+        
+        ax = fig.add_subplot(gs[i])
+        weights = np.ones_like(x_data[:,i]) / len(x_data[:,i])
+        ax.hist(x_data[:,i], weights=weights)
+        
+        # Calculate Gaussian least-square fitting process
         if gauss_c:
             param_optimised, _ = curve_fit(gaus,x_hist,y_hist,p0=[max(y_hist),mean,sigma],maxfev=5000)
-            
             # Plotting gaussian curve
             x_hist_2=np.linspace(np.min(x_hist),np.max(x_hist),500)
-            axs[i].plot(x_hist_2,gaus(x_hist_2,*param_optimised),'r.:',label='Gaussian fit')
+            ax.plot(x_hist_2,gaus(x_hist_2,*param_optimised),'r.:',label='Gaussian fit')
         
-        weights = np.ones_like(x_data[:,i]) / len(x_data[:,i])
-        axs[i].hist(x_data[:,i], weights=weights)
-        
-        axs[i].set_xlabel('Pixels')
-        axs[i].set_ylabel("Probability")
-        axs[i].set_title(f"'{idx_filt[:-1]}' - '{colname[i]}'")
-
+        ax.set_xlabel('Pixels')
+        ax.set_ylabel("Probability")
+        ax.set_title(f"'{idx_filt[:-1]}' - '{colname[i]}'")
+    fig.tight_layout()
+    
 # Main
 def main():
     args = parser.parse_args()
-
     calibrationFiles = sorted(glob.glob('./results/*.yml'), key=lambda x:[int(c) if c.isdigit() else c for c in re.split(r'(\d+)', x)])
     
     cc_data = {}
@@ -105,7 +119,10 @@ def main():
     df_ccd_2 = pd.concat([df_ccd, df_ccd_dcb])
     df_ccd_c = pd.concat([df_ccd_2, pd.DataFrame(cc_summary).T], axis=1)
     
-    df_histogram(df_ccd_c, ['fx', 'fy', 'cx', 'cy'], ('Filter by time,', 'summary'), gauss_c=True)
+    df_histogram(df_ccd_c, ['fx', 'fy', 'cx', 'cy'], ('Filter by time,', 'summary'), ('C42-', 'index'), gauss_c=False)
+    df_histogram(df_ccd_c, ['fx', 'fy', 'cx', 'cy'], ('Filter by distance,', 'summary'), ('C51-', 'index'))
+    df_histogram(df_ccd_c, ['fx', 'fy', 'cx', 'cy'], ('Filter by time and points,', 'summary'), gauss_c=True)
+    df_histogram(df_ccd_c, ['fx', 'fy', 'cx', 'cy'], ('C67-', 'index'), gauss_c=False)
     plt.show()
     
     #Por video, mismo tipo de error, al menos 3 casos
